b5aea65def093c1627be78c5984da8dfc64d6029
astrifasarii <waodeastrifasari@gmail.com>
2025-11-15 11:38:21 +0700

    update index.html

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const contacts = [
+  {
+    id: 1,
+    Name: "Mochamad Irvan",
+    PhoneNumber: 62881080070700,
+    email: "mchmdirvan@example.com",
+    location: "Jakarta",
+  },
+  {
+    id: 2,
+    Name: "Waode Astrifa Sari",
+    PhoneNumber: 62881080080800,
+    email: "adhitya@example.com",
+    location: "Bandung",
+  },
+];
+
+console.log(contacts);
